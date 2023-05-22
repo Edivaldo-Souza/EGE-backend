7f8ee000777dcb916b4481a5c3c809f08011e575
Edivaldo <edivaldoraimundo84@gmail.com>
2023-05-22 11:12:01 -0300

    third commit

--- src/main/java/com/edu/ege/EGE/controllers/EventoController.java
+++ src/main/java/com/edu/ege/EGE/controllers/EventoController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -66,6 +67,15 @@ public class EventoController {
 			return new ResponseEntity<>(desc,HttpStatus.OK);
 		}
 		else return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@DeleteMapping
+	public String delete(@Valid @RequestBody EventoDTO eve) {
+		Evento e = service.findById(0);
+		if(e!=null) {
+			return "evento deletado";
+		}
+		else return "incapaz de deletar evento";
 	}
 	
 	

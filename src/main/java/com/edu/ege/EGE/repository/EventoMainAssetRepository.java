48d7f82aec58edf0e282b3618836e8cfaa7acfc9
Edivaldo <edivaldoraimundo84@gmail.com>
2023-06-08 18:49:16 -0300

    prod

--- src/main/java/com/edu/ege/EGE/repository/EventoMainAssetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.edu.ege.EGE.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.edu.ege.EGE.entities.EventoMainAsset;
-
-public interface EventoMainAssetRepository extends JpaRepository<EventoMainAsset,Long>{
-	EventoMainAsset findByIdEvento(long id);
-}
